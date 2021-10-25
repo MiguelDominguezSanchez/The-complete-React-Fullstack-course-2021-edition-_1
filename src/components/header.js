299208e158b7f4e01e85b3e7affcb081d4fdf933
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-10-25 07:19:55 +0200

    2–005 Project structure

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+	const getTheYear = () => {
+		const newDate = new Date()
+		return newDate.getFullYear()
+	}
+
+	return <div>The date is {getTheYear()}</div>
+}
+
+export default Header
